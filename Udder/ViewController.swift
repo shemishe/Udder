99871178e9c69b30c59afee0510ad656d38ed60e
Sherman Shi <shermanmishi@icloud.com>
2019-01-15 22:22:36 -0800

    add view did  appear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -22,6 +22,10 @@ class ViewController: UIViewController {
     override func viewDidDisappear(_ animated: Bool) {
         //
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        //
+    }
 
 }
 
